5f6eb9c935b664fe7b841bff8f4943a5bca10570
Raphael Enriquez <raphael.enriquez@obf.ateneo.edu>
2021-05-19 18:34:30 +0800

    Ported FCFS and SJF into python

--- proj.py
+++ proj.py
@@ -1,9 +1,36 @@
+def get_details(process_list):
+    process_list.sort()
+    process_count = len(process_list)
+
+    total_waiting_time = 0
+    total_turnaround_time = 0
+    total_response_time = 0
+
+    print("Waiting times: ")
+    for p in process_list:
+        print(" Process ", p[0], ": ", p[4], "ns", sep="")
+        total_waiting_time += p[4]
+    print("Average waiting time: ", total_waiting_time/process_count, "ns", sep="")
+
+    print("Turnaround times: ")
+    for p in process_list:
+        print(" Process ", p[0], ": ", p[5], "ns", sep="")
+        total_turnaround_time += p[5]
+    print("Average turnaround time: ", total_turnaround_time/process_count, "ns", sep="")
+
+    print("Response times: ")
+    for p in process_list:
+        print(" Process ", p[0], ": ", p[6], "ns", sep="")
+        total_response_time += p[6]
+    print("Average response time: ", total_response_time/process_count, "ns", sep="")
+
+
 def FCFS(process_list):
     ready_queue = []
     terminated = []
+
     elapsed_time = 0
     completed = 0
-
     total_burst_time = 0
 
     process_count = len(process_list)
@@ -44,11 +71,67 @@ def FCFS(process_list):
         else:
             elapsed_time += 1 
 
-    # print(terminated)
-    print("Total time elapsed: ", elapsed_time, "ns")
-    print("Total CPU burst time: ", total_burst_time, "ns")
-    print("CPU Utilization: ", (total_burst_time/elapsed_time)*100, "%")
-    print("Throughput: ", (process_count/elapsed_time), "processes/ns")
+    print("Total time elapsed: ", elapsed_time, "ns", sep="")
+    print("Total CPU burst time: ", total_burst_time, "ns", sep="")
+    print("CPU Utilization: ", (total_burst_time/elapsed_time)*100, "%", sep="")
+    print("Throughput: ", (process_count/elapsed_time), "processes/ns", sep="")
+    get_details(terminated)
+
+def SJF(process_list):
+    ready_queue = []
+    terminated = []
+
+    elapsed_time = 0
+    completed = 0
+    total_burst_time = 0
+
+    process_count = len(process_list)
+
+    process_list = sorted(process_list, key=lambda x: x[2])
+
+
+    while completed != process_count:
+
+        # Checks if there are processes we can add to the ready queue
+        for process in process_list:
+            # If there are processes available to be added
+            # to the ready queue, then this moves 
+            # the process to the ready queue
+            if process[1] <= elapsed_time:
+                ready_queue.append(process)
+                process_list.remove(process)
+                break
+
+        if (len(ready_queue)!= 0):        
+            while (len(ready_queue)!= 0):
+                for p in ready_queue:
+                    if p[1] > elapsed_time:
+                        p[7] = p[1]
+                    else:
+                        p[7] = elapsed_time
+
+                    if p[1] < elapsed_time:
+                        p[4] = elapsed_time - p[1] 
+                    p[5] = p[4] + p[2]
+                    p[6] = p[4]
+
+                    elapsed_time += p[2]
+                    total_burst_time += p[2]
+
+                    print(p[7], " ", p[0], " ", p[2],"X", sep="")
+
+                    terminated.append(p)
+                    ready_queue.remove(p)
+
+                    completed += 1
+        else:
+            elapsed_time += 1 
+
+    print("Total time elapsed: ", elapsed_time, "ns", sep="")
+    print("Total CPU burst time: ", total_burst_time, "ns", sep="")
+    print("CPU Utilization: ", (total_burst_time/elapsed_time)*100, "%", sep="")
+    print("Throughput: ", (process_count/elapsed_time), "processes/ns", sep="")
+    get_details(terminated)
 
 
 if __name__ =="__main__":
@@ -64,10 +147,13 @@ if __name__ =="__main__":
             processes.append([j+1]+list(map(int, input().split()))
 +[0,0,0,0])
         
-        print(str(i+1), ". ", s[1], sep ="")
+        print(str(i+1), ". ", s[1], sep="")
 
         if (s[1]=="FCFS"):
             FCFS(processes)
+
+        elif(s[1] == "SJF"):
+            SJF(processes)
 
 
  
